0debc55615cbe10c531c7abec073795998f9a0ea
facundoaparicio <facundoaparicio@alu.frt.utn.edu.ar>
2022-01-10 16:38:15 -0300

    nuevos cambios acceso externo

--- AccesoExterno/Adaptadores/AdaptadorInventario.cs
+++ AccesoExterno/Adaptadores/AdaptadorInventario.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Presentador.Presentadores
+namespace AccesoExterno.Adaptadores
 {
     public class AdaptadorInventario
     {
